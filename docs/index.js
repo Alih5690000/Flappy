9bd98711481a0904f24c8c2b708d206d4be671a4
Alih5690000 <ffffffaaaaaa320@outlook.com>
2026-04-23 07:04:30 +0000

    Copied into docs again idk if works

--- docs/index.js
+++ docs/index.js
@@ -31,7 +31,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpnimpxm7a.js
+// include: /tmp/tmpt6jnmjqm.js
 if (!Module["expectedDataFileDownloads"]) Module["expectedDataFileDownloads"] = 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -324,7 +324,7 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: /tmp/tmpnimpxm7a.js
+// end include: /tmp/tmpt6jnmjqm.js
 var arguments_ = [];
 
 var thisProgram = "./this.program";
